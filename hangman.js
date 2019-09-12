cc871850937d3b527c7f9765305519ac1d8dee68
Prince Sumberia <broughtmeup@gmail.com>
2019-09-12 17:10:02 +0530

    refactored code

--- hangman.js
+++ hangman.js
@@ -1,66 +1,48 @@
 class Hangman {
-    constructor(word, remainingGuesses, guessedLetterArray = [], letterArray = [], guessingArray = []) {
-        this.word = word;
+    constructor(word, remainingGuesses, guessedLetters = [], letterArray = [], guessingArray = []) {
+        this.word = word.toLowerCase().split('');
         this.remainingGuesses = remainingGuesses;
-        this.guessedLetterArray = guessedLetterArray;
+        this.guessedLetters = guessedLetters;
         this.letterArray = letterArray;
         this.guessingArray = guessingArray;
     }
-    splitWord() {
-        const arrayWords = this.word.split(' ');
-        arrayWords.forEach((word) => {
-            this.letterArray = this.letterArray.concat(word.toLowerCase().split(''));
-        });
-        return this.letterArray;
-    }
-    startGame(game) {
-        game.splitWord().forEach((element) => {
-            this.guessingArray.push('*');
-        });
-        console.log(this.guessingArray);
-    }
-    printGuessingArray() {
-        let strElement = '';
-        this.guessingArray.forEach((element) => {
-            strElement = strElement.concat(element);
-        });
-        console.log(strElement);
-    }
-    guessedLetters(letter) {
-        if (this.remainingGuesses == 0) {
-            console.log(`No more guesses allowed.`);
-        } else {
-            this.guessedLetterArray.push(letter);
-            // const index = this.letterArray.findIndex(element => element === letter);
-            const indexes = searchIndex(this.letterArray, letter);
-            indexes.forEach((index) => {
-                this.guessingArray[index] = letter;
-            });
-            if (indexes.length == 0) {
-                this.remainingGuesses -= 1;
+
+    startGame() {
+        let puzzle = '';
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === " ") {
+                puzzle += letter;
+            } else {
+                puzzle += '*';
             }
-            console.log(this.guessingArray);
-            this.printGuessingArray();
-            return this.guessedLetterArray;
+        });
+        return puzzle;
+    }
+
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+
+        if (isUnique) {
+            this.guessedLetters.push(guess);
+        }
+
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--;
         }
     }
 }
 
-const searchIndex = (arr, val) => {
-    let indexes = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == val) {
-            indexes.push(i);
-        }
-    }
-    return indexes;
-};
+
 
 const game1 = new Hangman('Hello world', 5);
-game1.startGame(game1);
 window.addEventListener('keypress', (event) => {
     const guess = String.fromCharCode(event.charCode);
-    game1.guessedLetters(guess);
+    game1.makeGuess(guess);
+    console.log(game1.startGame());
+    console.log(game1.remainingGuesses);
+
 })
 
 // const newArray = game1.splitWord();
