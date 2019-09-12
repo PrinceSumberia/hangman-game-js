10a6524e088a5a92b869e63931f4691d0371dae9
Prince Sumberia <broughtmeup@gmail.com>
2019-09-12 15:29:33 +0530

    added basic functionality

--- hangman.js
+++ hangman.js
@@ -1,42 +1,68 @@
-const Hangman = function (word, remainingGuesses, guessedLetterArray = []) {
+const Hangman = function (word, remainingGuesses, guessedLetterArray = [], letterArray = [], guessingArray = []) {
     this.word = word;
     this.remainingGuesses = remainingGuesses;
     this.guessedLetterArray = guessedLetterArray;
-}
+    this.letterArray = letterArray;
+    this.guessingArray = guessingArray;
+};
 
 Hangman.prototype.splitWord = function () {
-    const arrayWords = this.word.split(" ");
-    let newArray = [];
-    arrayWords.forEach(word => {
-        newArray = newArray.concat(word.toLowerCase().split(''));
+    const arrayWords = this.word.split(' ');
+    arrayWords.forEach((word) => {
+        this.letterArray = this.letterArray.concat(word.toLowerCase().split(''));
     });
-    return newArray;
+    return this.letterArray;
+};
+
+Hangman.prototype.startGame = function (game) {
+    game.splitWord().forEach((element) => {
+        this.guessingArray.push("*");
+    });
+    console.log(this.guessingArray);
 }
+
+
+Hangman.prototype.printGuessingArray = function () {
+    let strElement = '';
+    this.guessingArray.forEach(element => {
+        strElement = strElement.concat(element)
+    });
+    console.log(strElement);
+};
 
 Hangman.prototype.guessedLetters = function (letter) {
     if (this.remainingGuesses == 0) {
-        console.log(`No more guesses are allowed. You guessed ${this.count} times`);
+        console.log(`No more guesses are allowed. You guessed ${this.remainingGuesses} times`);
     } else {
         this.remainingGuesses -= 1;
         this.guessedLetterArray.push(letter);
+        const index = this.letterArray.findIndex(element => element === letter);
+        if (index > -1) {
+            this.guessingArray[index] = letter;
+        }
+        // console.log(this.guessingArray);
+
+        this.printGuessingArray();
         return this.guessedLetterArray;
     }
-}
+};
 
 
-const game1 = new Hangman("Hello World", 5);
-console.log(game1.splitWord());
-console.log(game1.guessedLetters('a'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
-console.log(game1.guessedLetters('b'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
-console.log(game1.guessedLetters('c'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
-console.log(game1.guessedLetters('d'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
-console.log(game1.guessedLetters('e'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
-console.log(game1.guessedLetters('f'));
-console.log(`Word is ${game1.word} and Number of remaining guesses are ${game1.remainingGuesses}  and guessedArray is ${game1.guessedLetterArray}`);
+const game1 = new Hangman('Hello world', 5);
+game1.startGame(game1);
+game1.guessedLetters('w')
+game1.guessedLetters('o')
+game1.guessedLetters('l')
+game1.guessedLetters('d')
 
 
+
+
+// const newArray = game1.splitWord();
+
+// console.log(newArray);
+// console.log(game1.guessedLetters('b'));
+// console.log(game1.guessedLetters('c'));
+// console.log(game1.guessedLetters('d'));
+// console.log(game1.guessedLetters('e'));
+// console.log(game1.guessedLetters('f'));
